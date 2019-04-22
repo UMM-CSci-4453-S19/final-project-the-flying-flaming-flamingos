53b55eaece18dbbda504b3ac9315a3631beef5cf
Francisco Montanez <monta215@morris.umn.edu>
2019-04-22 16:46:09 -0500

    begin integrating neo4j

--- express.js
+++ express.js
@@ -1,43 +1,37 @@
-var credentials = require('./credentials.json'),
-  express = require('express'),
-  mysql = require("mysql"),
+var express = require('express'),
   app = express(),
   port = process.env.PORT || 1337;
+var neo4j = require("neo4j-driver");
+neo4j = neo4j.v1;
 
-credentials.host = 'ids.morris.umn.edu';
-var connection = mysql.createConnection(credentials);
+// find a way to use credentials
+var driver = neo4j.driver("bolt://csci-4409.morris.umn.edu:7687", neo4j.auth.basic("neo4j", "neo4j"));
 
-connection.connect(function(err) {
-  if (err) {
-    console.log("Problems with MySQL: " + err);
-  } else {
-    console.log("Connected to Database.");
-  }
-});
+
+var session = driver.session();
+
+session
+  .run("MATCH (n) RETURN COUNT(n)")
+  .subscribe({
+    onNext: function(record) {
+      console.log(record);
+    },
+    onCompleted: function() {
+      session.close();
+    },
+    onError: function(error) {
+      console.log(error);
+    }
+  });
 
 app.use(express.static(__dirname + '/public'));
 
 app.get("/click", function(req, res) {
-  var id = req.param('id');
-  var sql = 'YOUR SQL HERE'
-  console.log("Attempting sql ->" + sql + "<-");
-
-  connection.query(sql, (function(res) {
-    return function(err, rows, fields) {
-      if (err) {
-        console.log("We have an insertion error:");
-        console.log(err);
-      }
-      res.send(err); // Let the upstream guy know how it went
-    }
-  })(res));
+  //
 });
 
 app.get("/login", function(req, res) {
-  // getting the email entered in the form
-  var emailAddress = document.getElementById("authentication-form");
-  var sql = "SELECT EXISTS(SELECT 1 FROM users WHERE email=" + emailAddress + ");";
-  console.log("searching if email address exists in database...");
+  //
 });
 
 
