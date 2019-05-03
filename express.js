6265972dbf6b5f6b60d9b9df05e19f7429e1cf16
Francisco Montanez <monta215@morris.umn.edu>
2019-05-03 17:01:48 -0500

    work on signup/sign in function

--- express.js
+++ express.js
@@ -10,20 +10,22 @@ var session = driver.session();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/click", function (req, res) {
+app.get("/click", function(req, res) {
   //
 });
 
-app.get("/login", function (req, res) {
+app.get("/login", function(req, res) {
   //
 });
 
-app.get("/signup", function (req, res) {
+app.get("/signup", function(req, res) {
   console.log("i made it to signup on express.js")
   //
   var email = req.query['email'];
   var password = req.query['password'];
   var passwordRepeat = req.query['passwordRepeat'];
+  var flag = true;
+  var errorMessage;
   console.log(email, password, passwordRepeat)
 
   if (password != passwordRepeat) {
@@ -36,22 +38,30 @@ app.get("/signup", function (req, res) {
   } else {
     console.log("passwords match")
     session
-      .run("MERGE(n:user{email:'" + email + "',password:'" + password + "'}) ON CREATE SET n.email='" + email + "', n.password='" + password + "' ON MATCH RETURN count(*);")
+      .run("MATCH (n:user {email:'" + email + "'}) RETURN n")
       .subscribe({
-        onCompleted: function (record) {
-          if (record._fields[0].low > 0) {
+        onNext: function(record) {
+          flag = false;
+          console.log(record)
+          if (record._fields[0].properties.password == password) {
+
             console.log("user already exists")
-              .error(function () {
-                $scope.errorMessage = "User already exists!";
-              })
           } else {
-            console.log("Success!")
-              .success(function () { })
+            errorMessage = "You entered your password INCORRECTLY!";
           }
+          console.log(record._fields[0].properties.email)
+          console.log(record);
+        },
+        onCompleted: function(record) {
+
+          if (flag) {
+            session
+              .run("CREATE (n:user{email:'" + email + "', password:'" + password + "'})")
+          }
+          console.log("onCompleted");
           console.log(record);
         }
-      });
-
+      })
 
   }
 
@@ -63,7 +73,7 @@ app.get("/signup", function (req, res) {
   // });
 });
 
-app.get("/profile", function (req, res) {
+app.get("/profile", function(req, res) {
   res.send("../profile.html", {
     root: __dirname + '/public/'
   });
