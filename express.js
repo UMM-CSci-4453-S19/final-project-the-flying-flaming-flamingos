c0a742477baceac06e52d1697728ed6935d3d110
Francisco Montanez <monta215@morris.umn.edu>
2019-05-06 14:17:32 -0500

    work on linking user node and person node via relationship

--- express.js
+++ express.js
@@ -1,6 +1,6 @@
 var express = require('express'),
-    app = express(),
-    port = process.env.PORT || 1337;
+  app = express(),
+  port = process.env.PORT || 1337;
 var neo4j = require("neo4j-driver");
 neo4j = neo4j.v1;
 
@@ -10,86 +10,113 @@ var session = driver.session();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/login", function (req, res) {
-    var email = req.query['email'];
-    var password = req.query['password'];
-    var flag = true;
+app.get("/login", function(req, res) {
+  var email = req.query['email'];
+  var password = req.query['password'];
+  var flag = true;
 
+  session
+    .run("MATCH (n:user {email:'" + email + "'}) RETURN n")
+    .subscribe({
+      onNext: function(record) {
+        flag = false;
+        console.log(record);
+        if (record._fields[0].properties.password == password) {
+          res.status(200).send(email);
+        } else {
+          res.status(400).send("You entered your password INCORRECTLY!");
+          errorMessage = "You entered your password INCORRECTLY!";
+        }
+      },
+      onCompleted: function(record) {
+        if (flag) {
+          res.status(400).send("there is no profile with that email")
+        }
+      }
+    })
+
+
+});
+
+app.get("/signup", function(req, res) {
+  var email = req.query['email'];
+  var password = req.query['password'];
+  var passwordRepeat = req.query['passwordRepeat'];
+  var flag = true;
+
+  if (password != passwordRepeat) {
+    res.status(400).send("the passwords do not match");
+  } else {
     session
-        .run("MATCH (n:user {email:'" + email + "'}) RETURN n")
-        .subscribe({
-            onNext: function (record) {
-                flag = false;
-                console.log(record);
-                if (record._fields[0].properties.password == password) {
-                    res.status(200).send("success");
-                } else {
-                    res.status(400).send("You entered your password INCORRECTLY!");
-                    errorMessage = "You entered your password INCORRECTLY!";
+      .run("MATCH (n:user {email:'" + email + "'}) RETURN n")
+      .subscribe({
+        onNext: function(record) {
+          flag = false;
+        },
+        onCompleted: function(record) {
+
+          if (flag) {
+            session
+              .run("CREATE (n:user{email:'" + email + "', password:'" + password + "'})")
+              .subscribe({
+                onCompleted: function(n) {
+                  res.status(200).send(email)
                 }
-            },
-            onCompleted: function (record) {
-                if (flag) {
-                    res.status(400).send("there is no profile with that email")
-                }
-            }
-        })
-
-
+              });
+          } else {
+            res.status(400).send("profile already exists with given email");
+          }
+        }
+      })
+  }
 });
 
-app.get("/signup", function (req, res) {
-    var email = req.query['email'];
-    var password = req.query['password'];
-    var passwordRepeat = req.query['passwordRepeat'];
-    var flag = true;
-
-    if (password != passwordRepeat) {
-        res.status(400).send("the passwords do not match");
-    } else {
-        session
-            .run("MATCH (n:user {email:'" + email + "'}) RETURN n")
-            .subscribe({
-                onNext: function (record) {
-                    flag = false;
-                },
-                onCompleted: function (record) {
-
-                    if (flag) {
-                        session
-                            .run("CREATE (n:user{email:'" + email + "', password:'" + password + "'})")
-                            .subscribe({
-                                onCompleted: function (n) {
-                                    res.status(200).send("success")
-                                }
-                            });
-                    } else {
-                        res.status(400).send("profile already exists with given email");
-                    }
-                }
-            })
-    }
+app.get("/profile", function(req, res) {
+  res.status(200).sendFile("/profile/profile.html", {
+    root: __dirname + '/public/'
+  });
 });
 
-app.get("/profile", function (req, res) {
-    res.status(200).sendFile("/profile/profile.html", {root: __dirname + '/public/'});
+app.get("/match", function(req, res) {
+  var firstname = req.query['firstname'];
+  var lastname = req.query['lastname'];
+  var gender = req.query['gender'];
+  var records = [];
+
+  session.run("MATCH (n:person {gender:'" + gender + "'}) WHERE n.firstName =~ '(?i)" + firstname + ".*' AND n.lastName =~ '(?i)" + lastname + ".*' RETURN n")
+    .subscribe({
+      onNext: function(record) {
+        records.push(record);
+      },
+      onCompleted: function(record) {
+        res.status(200).send(records);
+      }
+    })
 });
 
-app.get("/match", function (req, res) {
-    var firstname = req.query['firstname'];
-    var lastname = req.query['lastname'];
-    var gender = req.query['gender'];
-    var records = [];
+app.get("/linkPerson", function(req, res) {
+  console.log("inside linkPerson in express")
+  var email = req.query['email'];
+  var firstname = req.query['firstname'];
+  var lastname = req.query['lastname'];
+  var gender = req.query['gender'];
+  var dob = req.query['dob'];
 
-    session.run("MATCH (n:person {gender:'" + gender + "'}) WHERE n.firstName =~ '(?i)" + firstname + ".*' AND n.lastName =~ '(?i)" + lastname + ".*' RETURN n")
-        .subscribe({
-            onNext: function (record) {
-                records.push(record);
-            },
-            onCompleted: function (record) {
-                res.status(200).send(records);
-            }
-        })
+  console.log("inside appget linkPerson");
+  console.log(email, firstname, lastname, gender, dob);
+
+  // creating person node
+  session.run("CREATE (m:person {firstName:'" + firstname + "',lastName:'" + lastname + "',gender:'" + gender + "',birthDate:'" + dob + "'})")
+  session.close();
+  session.run("MATCH (n:user {email:'" + email + "'}), (m:person {firstName:'" + firstname + "', lastName:'" + lastname + "'}) MERGE (n)-[r:isUser]->(m)")
+
+    .subscribe({
+      onCompleted: function() {
+        console.log("link person session onComplete i am here")
+      }
+    })
 });
+
+
 
 app.listen(port);
--- public/profile/profile.js
+++ public/profile/profile.js
@@ -1,49 +1,70 @@
 angular.module('profile', [])
-    .controller('profileCtrl', ProfileCtrl)
-    .factory('profileApi', profileApi)
-    .constant('apiUrl', 'http://localhost:1337'); //CHANGE for the lab!
+  .controller('profileCtrl', ProfileCtrl)
+  .factory('profileApi', profileApi)
+  .constant('apiUrl', 'http://localhost:1337'); //CHANGE for the lab!
 
 function ProfileCtrl($scope, profileApi) {
 
-    $scope.matchPerson = matchPerson;
-    var loading = false;
-    var firstname = "";
-    var lastName = "";
-    var gender;
-    var dob;
+  $scope.addToTree = addToTree;
+  $scope.matchPerson = matchPerson;
+  var loading = false;
+  var firstname = "";
+  var lastName = "";
+  var gender;
+  var dob;
 
-    function matchPerson() {
-        console.log("i made it matchPerson");
-        profileApi.MatchPerson()
-            .success(function (res) {
-                console.log(res);
-            })
-            .error(function (res) {
-                console.log(res);
-            })
-    }
+  function matchPerson() {
+    console.log("i made it matchPerson");
+    profileApi.MatchPerson()
+      .success(function(res) {
+        console.log(res);
+      })
+      .error(function(res) {
+        console.log(res);
+      })
+  }
+
+  function addToTree() {
+    console.log("i made it addToTree");
+    profileApi.AddToTree()
+    console.log("after profileApi.matchPerson in function addToTree profile.js");
+  }
 
 }
 
 
 function profileApi($http, apiUrl) {
-    return {
-        MatchPerson: function () {
-            console.log("im in profile");
-            firstname = window.document.getElementById("firstname").value;
-            lastName = window.document.getElementById("lastname").value;
-            gender = window.document.getElementsByName("gender")[0].checked;
-            console.log(gender)
+  return {
+    MatchPerson: function() {
+      console.log("im in profile");
+      firstname = window.document.getElementById("firstname").value;
+      lastName = window.document.getElementById("lastname").value;
+      gender = window.document.getElementsByName("gender")[0].checked;
+      console.log(gender)
 
-            if(gender){
-                gender = "Male";
-            }else {
-                gender = "Female";
-            }
+      if (gender) {
+        gender = "Male";
+      } else {
+        gender = "Female";
+      }
 
-            var url = apiUrl + '/match?firstname=' + firstname + "&lastname=" + lastName + '&gender=' + gender;
-            return $http.get(url);
-        }
+      var url = apiUrl + '/match?firstname=' + firstname + "&lastname=" + lastName + '&gender=' + gender;
+      return $http.get(url);
+    },
 
-    };
+    AddToTree: function() {
+      console.log("im in AddToTree");
+      firstname = window.document.getElementById("firstname").value;
+      lastName = window.document.getElementById("lastname").value;
+      gender = window.document.getElementsByName("gender")[0].checked;
+      dob = window.document.getElementById("dob").value;
+      email = window.localStorage.getItem("email");
+      console.log("in AddToTree values");
+      console.log(firstname, lastName, gender, dob, email);
+
+      var url = apiUrl + '/linkPerson?firstname=' + firstname + "&lastname=" + lastName + '&gender=' + gender + '&dob=' + dob + '&email=' + email;
+      return $http.get(url);
+    }
+
+  };
 }
