0225b3d733b263b9ded5bcd0d4152ba87dbbf289
Francisco Montanez <monta215@morris.umn.edu>
2019-05-06 14:18:57 -0500

    start with familySearch

---
+++ public/profile/familySearch.js
@@ -0,0 +1,62 @@
+<
+body >
+  <
+  div class = "wrap" >
+  <
+  p > Didn 't find anyone? Add them!</p> <
+  h5 > * Start with your parents * < /h5>
+
+  <
+  fieldset >
+  <
+  legend > Relative 's Personal information:</legend>
+
+First Name:
+  <
+  input type = "text"
+id = "firstname"
+placeholder = "Enter first name"
+required > < br > < br >
+  Last Name:
+  <
+  input type = "text"
+id = "lastname"
+placeholder = "Enter last name"
+required > < br > < br >
+
+  <
+  form >
+  Gender: < br >
+  <
+  input type = "radio"
+name = "gender"
+id = "gender-male"
+value = "male" > Male < br >
+  <
+  input type = "radio"
+name = "gender"
+id = "gender-female"
+value = "female" > Female < br > < br >
+  <
+  /form>
+
+  <
+  form >
+  Relation to me: < br >
+  <
+  input type = "radio"
+name = "gender"
+id = "gender-male"
+value = "male" > This is my parent < br >
+  <
+  input type = "radio"
+name = "gender"
+id = "gender-female"
+value = "female" > This is my child < br > < br >
+  <
+  /form>
+
+  <
+  /fieldset><br> <
+  /div> <
+  /body>
--- public/script.js
+++ public/script.js
@@ -1,54 +1,56 @@
 angular.module('buttons', [])
-    .controller('buttonCtrl', ButtonCtrl)
-    .factory('buttonApi', buttonApi)
-    .constant('apiUrl', 'http://localhost:1337'); //CHANGE for the lab!
+  .controller('buttonCtrl', ButtonCtrl)
+  .factory('buttonApi', buttonApi)
+  .constant('apiUrl', 'http://localhost:1337'); //CHANGE for the lab!
 
 function ButtonCtrl($scope, buttonApi) {
-    $scope.login = login;
-    $scope.signup = signup;
+  $scope.login = login;
+  $scope.signup = signup;
 
-    // login function
-    function login() {
-        buttonApi.logIn($scope.user)
-            .success(function (res) {
-                location.assign("http://localhost:1337/profile");
-            })
-            .error(function (res) {
-                alert(res);
-            });
-    }
+  // login function
+  function login() {
+    buttonApi.logIn($scope.user)
+      .success(function(res) {
+        window.localStorage.setItem("email", res);
+        location.assign("http://localhost:1337/profile");
+      })
+      .error(function(res) {
+        alert(res);
+      });
+  }
 
-    // signup function
-    function signup() {
-        console.log("i made it to lowercase signup");
-        buttonApi.signUp()
-            .success(function (res) {
-                location.assign("http://localhost:1337/profile");
-            })
-            .error(function (res) {
-                alert(res);
-            });
-    }
+  // signup function
+  function signup() {
+    console.log("i made it to lowercase signup");
+    buttonApi.signUp()
+      .success(function(res) {
+        window.localStorage.setItem("email", res);
+        location.assign("http://localhost:1337/profile");
+      })
+      .error(function(res) {
+        alert(res);
+      });
+  }
 }
 
 function buttonApi($http, apiUrl) {
-    return {
-        // logIn function
-        logIn: function (user) {
-            var email = window.document.getElementById("login-email").value;
-            var password = window.document.getElementById("login-psw").value;
-            var url = apiUrl + '/login' + '?email=' + email + "&password=" + password;
-            return $http.get(url);
-        },
+  return {
+    // logIn function
+    logIn: function(user) {
+      var email = window.document.getElementById("login-email").value;
+      var password = window.document.getElementById("login-psw").value;
+      var url = apiUrl + '/login' + '?email=' + email + "&password=" + password;
+      return $http.get(url);
+    },
 
-        // signUp function
-        signUp: function () {
-            var email = window.document.getElementById("signup-email").value;
-            var password = window.document.getElementById("signup-psw").value;
-            var passwordRepeat = window.document.getElementById("signup-repeat-psw").value;
-            var url = apiUrl + '/signup?email=' + email + '&password=' + password + '&passwordRepeat=' + passwordRepeat;
-            return $http.get(url);
-        }
+    // signUp function
+    signUp: function() {
+      var email = window.document.getElementById("signup-email").value;
+      var password = window.document.getElementById("signup-psw").value;
+      var passwordRepeat = window.document.getElementById("signup-repeat-psw").value;
+      var url = apiUrl + '/signup?email=' + email + '&password=' + password + '&passwordRepeat=' + passwordRepeat;
+      return $http.get(url);
+    }
 
-    };
+  };
 }
