ce9aff66fbd65a01ee802360f05d690964f6a25e
deglm006 <deglm006@morris.umn.edu>
2019-05-06 09:30:28 -0500

    Reformat code

--- express.js
+++ express.js
@@ -1,6 +1,6 @@
 var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 1337;
+    app = express(),
+    port = process.env.PORT || 1337;
 var neo4j = require("neo4j-driver");
 neo4j = neo4j.v1;
 
@@ -10,12 +10,12 @@ var session = driver.session();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get("/click", function(req, res) {
-  //
+app.get("/click", function (req, res) {
+    //
 });
 
-app.get("/login", function(req, res) {
-  //
+app.get("/login", function (req, res) {
+    //
 
     var email = req.query['email'];
     var password = req.query['password'];
@@ -24,91 +24,91 @@ app.get("/login", function(req, res) {
     console.log(email, password);
 
 
+    console.log("passwords match");
+    session
+        .run("MATCH (n:user {email:'" + email + "'}) RETURN n")
+        .subscribe({
+            onNext: function (record) {
+                flag = false;
+                console.log(record);
+                if (record._fields[0].properties.password == password) {
+                    res.status(200).send("success");
+                    console.log("logging in")
+                } else {
+                    res.status(400).send("You entered your password INCORRECTLY!");
+                    errorMessage = "You entered your password INCORRECTLY!";
+                }
+
+            },
+            onCompleted: function (record) {
+                if (flag) {
+                    res.status(400).send("there is no profile with that email")
+                }
+            }
+        })
+
+
+});
+
+app.get("/signup", function (req, res) {
+    console.log("i made it to signup on express.js");
+    //
+    var email = req.query['email'];
+    var password = req.query['password'];
+    var passwordRepeat = req.query['passwordRepeat'];
+    var flag = true;
+    var errorMessage;
+    console.log(email, password, passwordRepeat);
+
+    if (password != passwordRepeat) {
+
+        res.status(400).send("the passwords do not match");
+
+        console.log("the passwords do not match");
+
+        // $scope.errorMessage = "Passwords do NOT match!";
+        console.log("Hey! you are in the if password statement")
+
+    } else {
         console.log("passwords match");
         session
             .run("MATCH (n:user {email:'" + email + "'}) RETURN n")
             .subscribe({
-                onNext: function(record) {
+                onNext: function (record) {
                     flag = false;
-                    console.log(record);
-                    if (record._fields[0].properties.password == password) {
-                        res.status(200).send("success");
-                        console.log("logging in")
-                    } else {
-                        res.status(400).send("You entered your password INCORRECTLY!");
-                        errorMessage = "You entered your password INCORRECTLY!";
-                    }
 
                 },
                 onCompleted: function (record) {
-                    if(flag){
-                        res.status(400).send("there is no profile with that email")
+
+                    if (flag) {
+                        session
+                            .run("CREATE (n:user{email:'" + email + "', password:'" + password + "'})")
+                            .subscribe({
+                                onCompleted: function (n) {
+                                    console.log(n);
+                                    res.status(200).send("success")
+                                    //.sendFile("/profile/profile.html",{root: __dirname + '/public/'});
+                                    //console.log(res);
+                                }
+                            });
+
+                    } else {
+                        res.status(400).send("profile already exists with given email");
                     }
+                    console.log("onCompleted");
+                    console.log(record);
                 }
             })
 
-
+    }
 });
 
-app.get("/signup", function(req, res) {
-  console.log("i made it to signup on express.js");
-  //
-  var email = req.query['email'];
-  var password = req.query['password'];
-  var passwordRepeat = req.query['passwordRepeat'];
-  var flag = true;
-  var errorMessage;
-  console.log(email, password, passwordRepeat);
-
-  if (password != passwordRepeat) {
-
-      res.status(400).send("the passwords do not match");
-
-    console.log("the passwords do not match");
-
-    // $scope.errorMessage = "Passwords do NOT match!";
-    console.log("Hey! you are in the if password statement")
-
-  } else {
-    console.log("passwords match");
-    session
-      .run("MATCH (n:user {email:'" + email + "'}) RETURN n")
-      .subscribe({
-        onNext: function(record) {
-          flag = false;
-
-        },
-        onCompleted: function(record) {
-
-          if (flag) {
-            session
-              .run("CREATE (n:user{email:'" + email + "', password:'" + password + "'})")
-                .subscribe({
-                    onCompleted: function (n) {
-                      console.log(n);
-                        res.status(200).send("success")
-                            //.sendFile("/profile/profile.html",{root: __dirname + '/public/'});
-                        //console.log(res);
-                    }
-                });
-
-          }else {
-              res.status(400).send("profile already exists with given email");
-          }
-          console.log("onCompleted");
-          console.log(record);
-        }
-      })
-
-  }
-});
-
-app.get("/profile", function(req, res) {
+app.get("/profile", function (req, res) {
     // res.send("../profile.html", {
     //   root: __dirname + '/public/'
     // });
     console.log("in profile api");
-    res.status(200).sendFile("/profile/profile.html",{root: __dirname + '/public/'});
+    res.status(200).sendFile("/profile/profile.html", {root: __dirname + '/public/'});
     console.log("finished profile api");
 });
 
--- public/script.js
+++ public/script.js
@@ -1,81 +1,81 @@
 angular.module('buttons', [])
-  .controller('buttonCtrl', ButtonCtrl)
-  .factory('buttonApi', buttonApi)
-  .constant('apiUrl', 'http://localhost:1337'); //CHANGE for the lab!
+    .controller('buttonCtrl', ButtonCtrl)
+    .factory('buttonApi', buttonApi)
+    .constant('apiUrl', 'http://localhost:1337'); //CHANGE for the lab!
 
 function ButtonCtrl($scope, buttonApi) {
-  $scope.login = login;
-  $scope.signup = signup;
+    $scope.login = login;
+    $scope.signup = signup;
 
-  // login function
-  function login() {
-    buttonApi.logIn($scope.user)
-      .success(function(res) {
-        if (res == "success") {
-          window.location.assign("http://localhost:1337/profile")
-        }
-      })
-      .error(function (res) {
-        alert(res);
-        $scope.errorMessage = "Unable to login";
-      });
-  }
+    // login function
+    function login() {
+        buttonApi.logIn($scope.user)
+            .success(function (res) {
+                if (res == "success") {
+                    window.location.assign("http://localhost:1337/profile")
+                }
+            })
+            .error(function (res) {
+                alert(res);
+                $scope.errorMessage = "Unable to login";
+            });
+    }
 
-  // signup function
-  function signup() {
-    console.log("i made it to lowercase signup");
-    buttonApi.signUp()
-      .success(function(res) {
-        console.log(res);
-        if (res == "success") {
-          //buttonApi.profile()
-          window.location.assign("http://localhost:1337/profile")
-        }
-        console.log("made it to success in signup!")
-      })
-      .error(function (res) {
-        alert(res);
-        $scope.errorMessage = "Unable to sign up!";
-      });
-  }
+    // signup function
+    function signup() {
+        console.log("i made it to lowercase signup");
+        buttonApi.signUp()
+            .success(function (res) {
+                console.log(res);
+                if (res == "success") {
+                    //buttonApi.profile()
+                    window.location.assign("http://localhost:1337/profile")
+                }
+                console.log("made it to success in signup!")
+            })
+            .error(function (res) {
+                alert(res);
+                $scope.errorMessage = "Unable to sign up!";
+            });
+    }
 
 }
 
 
 function buttonApi($http, apiUrl) {
-  return {
-    getButtons: function() {
-      var url = apiUrl + '/buttons';
-      return $http.get(url);
-    },
-    // clickButton
-    clickButton: function(id) {
-      var url = apiUrl + '/click?id=' + id;
-      //      console.log("Attempting with "+url);
-      return $http.get(url); // Easy enough to do this way
-    },
+    return {
+        getButtons: function () {
+            var url = apiUrl + '/buttons';
+            return $http.get(url);
+        },
+        // clickButton
+        clickButton: function (id) {
+            var url = apiUrl + '/click?id=' + id;
+            //      console.log("Attempting with "+url);
+            return $http.get(url); // Easy enough to do this way
+        },
 
-    // logIn function
-    logIn: function(user) {
-      var email = window.document.getElementById("login-email").value;
-      var password = window.document.getElementById("login-psw").value;
-      var url = apiUrl + '/login' + '?email=' + email + "&password=" + password;
-      //      console.log("Attempting with "+url);
-      return $http.get(url); // Easy enough to do this way
-    },
+        // logIn function
+        logIn: function (user) {
+            var email = window.document.getElementById("login-email").value;
+            var password = window.document.getElementById("login-psw").value;
+            var url = apiUrl + '/login' + '?email=' + email + "&password=" + password;
+            //      console.log("Attempting with "+url);
+            return $http.get(url); // Easy enough to do this way
+        },
 
-    // signUp function
-    signUp: function() {
-      console.log("i made it to uppercase signUp");
-      var email = window.document.getElementById("signup-email").value;
-      var password = window.document.getElementById("signup-psw").value;
-      var passwordRepeat = window.document.getElementById("signup-repeat-psw").value;
-      var url = apiUrl + '/signup?email=' + email + '&password=' + password + '&passwordRepeat=' + passwordRepeat;
-      console.log(url);
-      console.log($http);
-      console.log("HEY! Youre at the bottom of button api signUp");
-      return $http.get(url);
-    }
+        // signUp function
+        signUp: function () {
+            console.log("i made it to uppercase signUp");
+            var email = window.document.getElementById("signup-email").value;
+            var password = window.document.getElementById("signup-psw").value;
+            var passwordRepeat = window.document.getElementById("signup-repeat-psw").value;
+            var url = apiUrl + '/signup?email=' + email + '&password=' + password + '&passwordRepeat=' + passwordRepeat;
+            console.log(url);
+            console.log($http);
+            console.log("HEY! Youre at the bottom of button api signUp");
+            return $http.get(url);
+        }
 
-  };
+    };
 }
